89aaa9f0cfda9c97a17a6ab91e9a73a3409cc6e4
Marcos Santos <marcospaulo1262@gmail.com>
2024-02-14 19:26:56 -0300

    Initial Commit

---
+++ ZapLink/ZapLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZapLinkApp.swift
+//  ZapLink
+//
+//  Created by Marcos Santos on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ZapLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
